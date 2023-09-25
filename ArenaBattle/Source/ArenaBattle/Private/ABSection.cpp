bad01086ca2ef2af48e9cf22f64822ce145b334b
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2023-09-25 23:29:54 +0900

    [아레나/모듈I/야준서] 스테이지 구현

---
+++ ArenaBattle/Source/ArenaBattle/Private/ABSection.cpp
@@ -0,0 +1,113 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABSection.h"
+
+// Sets default values
+AABSection::AABSection()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	RootComponent = Mesh;
+	
+	FString AssetPath = TEXT("/Script/Engine.StaticMesh'/Game/ArenaBattle/StaticMesh/SM_SQUARE.SM_SQUARE'");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_SQUARE(*AssetPath);
+	if(SM_SQUARE.Succeeded())
+	{
+		Mesh->SetStaticMesh(SM_SQUARE.Object);
+	}
+
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("TRIGGER"));
+	Trigger->SetBoxExtent(FVector(775.0f, 775.0f, 300.0f));
+	Trigger->SetupAttachment(RootComponent);
+	Trigger->SetRelativeLocation(FVector(0.0f, 0.0f, 250.0f));
+	Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+	
+	FString GateAssetPath = TEXT("/Script/Engine.StaticMesh'/Game/ArenaBattle/StaticMesh/SM_GATE.SM_GATE'");
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Gate(*GateAssetPath);
+	if(SM_Gate.Succeeded())
+	{
+		static FName GateSockets[] = {{TEXT("+XGate")}, {TEXT("-XGate")}, {TEXT("+YGate")}, {TEXT("-YGate")}};
+		for (auto GateSocket : GateSockets)
+		{
+			ABCHECK(Mesh->DoesSocketExist(GateSocket));
+			UStaticMeshComponent* NewGate = CreateDefaultSubobject<UStaticMeshComponent>(*GateSocket.ToString());
+			NewGate->SetStaticMesh(SM_Gate.Object);
+			NewGate->SetupAttachment(RootComponent, GateSocket);
+			NewGate->SetRelativeLocation(FVector(0.0f, -80.5f, 0.0f));
+			GateMeshes.Add(NewGate);
+
+			UBoxComponent* NewGateTrigger = CreateDefaultSubobject<UBoxComponent>(*GateSocket.ToString().Append(TEXT("Trigger")));
+			NewGateTrigger->SetBoxExtent(FVector(100.0f, 100.0f, 300.0f));
+			NewGateTrigger->SetupAttachment(RootComponent, GateSocket);
+			NewGateTrigger->SetRelativeLocation(FVector(70.0f, 0.0f, 250.0f));
+			NewGateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+			GateTriggers.Add(NewGateTrigger);
+		}
+	}
+
+	bNoBattle = false;
+}
+
+// Called when the game starts or when spawned
+void AABSection::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetState(bNoBattle ? ESectionState::COMPLETE : ESectionState::READY);
+}
+
+void AABSection::SetState(ESectionState NewState)
+{
+	switch (NewState)
+	{
+		case ESectionState::READY:
+			Trigger->SetCollisionProfileName(TEXT("ABTrigger"));
+			for (UBoxComponent* GateTrigger : GateTriggers)
+			{
+				GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+			}
+
+			OperateGates(true);
+			break;
+		case ESectionState::BATTLE:
+			Trigger->SetCollisionProfileName(TEXT("NoCollision"));
+			for (UBoxComponent* GateTrigger : GateTriggers)
+			{
+				GateTrigger->SetCollisionProfileName(TEXT("NoCollision"));
+			}
+
+			OperateGates(false);
+			break;
+		case ESectionState::COMPLETE:
+			Trigger->SetCollisionProfileName(TEXT("NoCollision"));
+			for (UBoxComponent* GateTrigger : GateTriggers)
+			{
+				GateTrigger->SetCollisionProfileName(TEXT("ABTrigger"));
+			}
+
+			OperateGates(true);
+			break;
+		default: ;
+	}
+
+	CurrentState = NewState;
+}
+
+void AABSection::OperateGates(bool bOpen)
+{
+	for(UStaticMeshComponent* Gate : GateMeshes)
+	{
+		Gate->SetRelativeRotation(bOpen ? FRotator(0.0f, -90.0f, 0.0f) : FRotator::ZeroRotator);
+	}
+}
+
+// Called every frame
+void AABSection::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
